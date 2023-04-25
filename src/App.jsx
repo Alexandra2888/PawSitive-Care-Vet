65d54346bcaddbeb928685c7aa750c6f17a9847a
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-04-25 13:44:20 +0300

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/home/Home'
 import Contact from './pages/contact/Contact'
 
 import NotFound from './pages/notFound/NotFound'
-import Navbar from './components/Navbar'
+import Navbar from './UI/Navbar';
 import Footer from './components/Footer'
 
 
--- src/UI/Footer.jsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-    return <h2>Footer</h2>
-}
-
-export default Footer;
