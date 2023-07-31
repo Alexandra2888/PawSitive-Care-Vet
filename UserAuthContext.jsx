261fc8a2356d96433f5185fdf188f9d55190b7fa
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-07-31 10:29:35 +0300

    fix css bug

---
+++ UserAuthContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "firebase/auth";
+import { auth } from "firebase/auth";
+const userAuthContext = createContext();
+
+export function UserAuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+  function register(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+  function logOut() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
+      console.log("Auth", currentuser);
+      setUser(currentuser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <userAuthContext.Provider
+      value={{ user, login, register, logOut }}
+    >
+      {children}
+    </userAuthContext.Provider>
+  );
+}
+
+export function useUserAuth() {
+  return useContext(userAuthContext);
+}
