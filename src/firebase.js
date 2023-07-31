68c32808046728a9939d1784e3d48224a6ee3605
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-07-31 11:48:02 +0300

    update firebase file

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkel-OjVnnqBAqso7bsU_jtv4jtcEI9xs",
+  authDomain: "pawsitive-1001d.firebaseapp.com",
+  projectId: "pawsitive-1001d",
+  storageBucket: "pawsitive-1001d.appspot.com",
+  messagingSenderId: "643389248154",
+  appId: "1:643389248154:web:53a7abe103327880880523"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
