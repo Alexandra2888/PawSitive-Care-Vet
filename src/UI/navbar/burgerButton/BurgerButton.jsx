31602269123960d2771051cd7f0a46fe776c87fe
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-08-27 19:34:00 +0300

    fix css for burger btn

--- src/UI/navbar/burgerButton/BurgerButton.jsx
+++ src/UI/navbar/burgerButton/BurgerButton.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
-import './BurgerButton.scss'; 
+import React, { useState } from "react";
+import "./BurgerButton.scss";
 import { NavLink } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
-import ToggleTheme from '../../../dark-mode/ToggleTheme';
+import { useNavigate } from "react-router-dom";
+import ToggleTheme from "../../../dark-mode/ToggleTheme";
 import { FiLogOut } from "react-icons/fi";
-import {useUserAuth } from "../../../../contexts/UserAuthContext";
-
+import { useUserAuth } from "../../../../contexts/UserAuthContext";
 
 const BurgerButton = () => {
-
-
   const { logOut, user } = useUserAuth();
   const navigate = useNavigate();
 
@@ -31,51 +28,50 @@ const BurgerButton = () => {
   return (
     <div>
       <button
-        className={`burger-button ${burgerMenuIsOpen ? 'open' : ''}`}
+        className={`burger-button ${burgerMenuIsOpen ? "open" : ""}`}
         onClick={toggleBurgerMenu}
       >
         <div
           className={`burger-line line-1 ${
-            burgerMenuIsOpen ? 'rotate-up' : ''
+            burgerMenuIsOpen ? "rotate-up" : ""
           }`}
         ></div>
         <div
-          className={`burger-line line-2 ${
-            burgerMenuIsOpen ? 'fade-out' : ''
-          }`}
+          className={`burger-line line-2 ${burgerMenuIsOpen ? "fade-out" : ""}`}
         ></div>
         <div
           className={`burger-line line-3 ${
-            burgerMenuIsOpen ? 'rotate-down' : ''
+            burgerMenuIsOpen ? "rotate-down" : ""
           }`}
         ></div>
       </button>
 
       {burgerMenuIsOpen && (
         <div className="menu-content">
-           <ul id="nav__items">
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/appointments">Appointments</NavLink>
-          </li>
-          <li>
-            <NavLink to="/shop" >
-             Shop
-            </NavLink>
-          </li>
-          {user && (
+          <ul id="nav__items">
             <li>
-              <button onClick={handleLogout} className="svg btn-primary">
-                <FiLogOut />
-              </button>
+              <NavLink to="/">Home</NavLink>
             </li>
-          )}
-          <li>
-            <ToggleTheme />
-          </li>
-        </ul>
+            <li>
+              <NavLink to="/profile">Profile</NavLink>
+            </li>
+            <li>
+              <NavLink to="/shop">Shop</NavLink>
+            </li>
+            <li>
+              <NavLink to="/appointments">Appointments</NavLink>
+            </li>
+            {user && (
+              <li>
+                <button onClick={handleLogout} className="svg btn-primary">
+                  <FiLogOut />
+                </button>
+              </li>
+            )}
+            <li>
+              <ToggleTheme className="dark-toggle" />
+            </li>
+          </ul>
         </div>
       )}
     </div>
